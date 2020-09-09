3136084b77d5aefa15aa679c0dc490b8628b9785
Fernando Pinho <fpinho@gmail.com>
2020-09-08 19:18:05 -0700

    Refactor and document command_robot service

--- project2/ball_chaser/src/drive_bot.cpp
+++ project2/ball_chaser/src/drive_bot.cpp
@@ -2,34 +2,55 @@
 #include "geometry_msgs/Twist.h"
 #include "ball_chaser/DriveToTarget.h"
 
-ros::Publisher motor_command_publisher;
-
-bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
+/**
+ *  DriveBot class creates a service that receives linear and angular velocities
+ *  and apply them to the robot by publishing to the differential_drive_controller
+ *  plugin command topic
+ */
+class DriveBot
 {
-    ROS_INFO("Command Robot received: linear_x:%1.2f / angular_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
+public:
+    DriveBot()
+    {
+        // Create a publisher to the "/cmd_vel" topic
+        motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    geometry_msgs::Twist motor_command;
+        // Define a "command_robot" service
+        service = n.advertiseService("/ball_chaser/command_robot", &DriveBot::handle_drive_request, this);
+    }
 
-    motor_command.linear.x = req.linear_x;
-    motor_command.angular.z = req.angular_z;
+private:
+    // "command_robot" service callback
+    bool handle_drive_request(ball_chaser::DriveToTarget::Request & req, ball_chaser::DriveToTarget::Response & res)
+    {
+        ROS_INFO("Command Robot received: linear_x:%1.2f / angular_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
 
-    motor_command_publisher.publish(motor_command);
+        // Build velocity message from service parameters
+        geometry_msgs::Twist motor_command;
+        motor_command.linear.x = req.linear_x;
+        motor_command.angular.z = req.angular_z;
 
-    res.msg_feedback = "Robot speed set - x: " + std::to_string(motor_command.linear.x) + " , z: " + std::to_string(motor_command.angular.z);
-    ROS_INFO_STREAM(res.msg_feedback);
+        // Publish velocity message
+        motor_command_publisher.publish(motor_command);
 
-    return true;
-}
+        // Return a response message
+        res.msg_feedback = "Robot speed set - x: " + std::to_string(motor_command.linear.x) + " , z: " + std::to_string(motor_command.angular.z);
 
-int main(int argc, char** argv)
+        ROS_INFO_STREAM(res.msg_feedback);
+
+        return true;
+    }
+
+    ros::NodeHandle n;
+    ros::Publisher motor_command_publisher;
+    ros::ServiceServer service;
+};
+
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "drive_bot");
 
-    ros::NodeHandle n;
-
-    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-
-    ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
+    DriveBot driveBot;
 
     ros::spin();
 
