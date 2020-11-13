b5419d6e7b958b65723cba9bdc1234aa0d43f2ab
Fernando Pinho <fpinho@gmail.com>
2020-11-12 20:19:21 -0800

    Update add markers script to reflect the home service solution

--- add_markers.sh
+++ add_markers.sh
@@ -14,3 +14,15 @@ xterm -e "roslaunch add_markers view_navigation_marker.launch" &
 # Add Rviz markers
 sleep 5
 xterm -e "rosrun add_markers add_markers_node" &
+
+sleep 5
+
+# Publish markers
+rosservice call /add_markers/drop_object "x: 3.5
+y: 4.0"
+sleep 5
+rosservice call /add_markers/pick_object "x: 3.5 
+y: 4.0"
+sleep 5
+rosservice call /add_markers/drop_object "x: 3.0 
+y: 0.0"
